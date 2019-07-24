36a3276533effa627821ea29dc897f7f18f6e437
Mike <mike.huang.mikank@gmail.com>
2019-07-24 11:27:58 +0800

    Seperate the controller from routes

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
-const crypto = require('crypto')
+
+// Include controller
+const convertController = require('./controllers/convert')
 
 const port = 3000
 
@@ -35,46 +37,9 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
-app.get('/convert', async (req, res) => {
-  const inputUrl = req.query.url
-  try {
-    // check if url exists in database
-    const urlResult = await Url.findOne({ originalUrl: inputUrl })
-    // if url already exist
-    if (urlResult) { return res.status(200).json({ url: urlResult.shortenedUrl }) }
+app.get('/convert', convertController.getShortened)
 
-    // generate an unique shortened url
-    let shortenedUrl = ''
-
-    while (true) {
-      // generate shortened url
-      shortenedUrl = crypto.randomBytes(Math.ceil((5 * 3) / 4)).toString('base64').replace(/\+/g, '0').replace(/\//g, '0').slice(0, 5)
-      // check if this url is unique
-      const url = await Url.findOne({ shortenedUrl: shortenedUrl })
-      if (!url) break
-    }
-
-    // create new url document
-    await Url.create({
-      originalUrl: inputUrl,
-      shortenedUrl
-    })
-    // redirect back to landing page
-    res.status(200).json({ url: shortenedUrl })
-  } catch (error) {
-    console.log(error)
-  }
-})
-
-app.get('/:shortenedUrl', (req, res) => {
-  Url.findOne({ shortenedUrl: req.params.shortenedUrl })
-    .then(data => {
-      // no such url
-      if (!data) { return res.redirect('/') }
-      // url found, redirect to original url page
-      res.redirect(data.originalUrl)
-    })
-})
+app.get('/:shortenedUrl', convertController.getOriginal)
 
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
---
+++ controllers/convert.js
@@ -0,0 +1,45 @@
+const crypto = require('crypto')
+// Include user model
+const Url = require('../models/url')
+
+module.exports = {
+  getShortened: async (req, res) => {
+    const inputUrl = req.query.url
+    try {
+      // check if url exists in database
+      const urlResult = await Url.findOne({ originalUrl: inputUrl })
+      // if url already exist
+      if (urlResult) { return res.status(200).json({ url: urlResult.shortenedUrl }) }
+
+      // generate an unique shortened url
+      let shortenedUrl = ''
+
+      while (true) {
+        // generate shortened url
+        shortenedUrl = crypto.randomBytes(Math.ceil((5 * 3) / 4)).toString('base64').replace(/\+/g, '0').replace(/\//g, '0').slice(0, 5)
+        // check if this url is unique
+        const url = await Url.findOne({ shortenedUrl: shortenedUrl })
+        if (!url) break
+      }
+
+      // create new url document
+      await Url.create({
+        originalUrl: inputUrl,
+        shortenedUrl
+      })
+      // redirect back to landing page
+      res.status(200).json({ url: shortenedUrl })
+    } catch (error) {
+      console.log(error)
+    }
+  },
+  getOriginal: (req, res) => {
+    Url.findOne({ shortenedUrl: req.params.shortenedUrl })
+      .then(data => {
+        // no such url
+        if (!data) { return res.redirect('/') }
+        // url found, redirect to original url page
+        res.redirect(data.originalUrl)
+      })
+  }
+}
